6328d9463be2a960ed2d7039e6165b38caab836b
Rafał Jankowski <rafaljankowski7@gmail.com>
2025-05-03 10:33:55 +0200

    Squashed 'ai-agent-service/' content from commit b73117f
    git-subtree-dir: ai-agent-service
    git-subtree-split: b73117f75132108937e5772e0942a99c840e057b

---
+++ src/main/kotlin/com/rj/ecommerce_ai_agent/EcommerceAiAgentApplication.kt
@@ -0,0 +1,11 @@
+package com.rj.ecommerce_ai_agent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceAiAgentApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceAiAgentApplication>(*args)
+}
---
+++ src/test/kotlin/com/rj/ecommerce_ai_agent/EcommerceAiAgentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rj.ecommerce_ai_agent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceAiAgentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
